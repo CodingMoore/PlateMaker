a8548c66677a8731090e89c0da5303569419d17c
Randel M <Randel.C.Moore@gmail.com>
2022-03-14 06:13:38 -0700

    Adds Title to HTML Files

--- PlateMaker/Plate.cs
+++ PlateMaker/Plate.cs
@@ -375,6 +375,7 @@ namespace PlateMaker
                 "<!DOCTYPE html>   \n" +
                 "<html lang='en'>   \n" +
                 "<head>   \n" +
+               $"\t<title>Plate {this.FileName}</title>   \n" +
                 "\t<meta charset='UTF-8'>   \n" +
                 "\t<meta http-equiv='X-UA-Compatible' content='IE=edge'>   \n" +
                 "\t<meta name='viewport' content='width=device-width, initial-scale=1.0'>   \n" +
