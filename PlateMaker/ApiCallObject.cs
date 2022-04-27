67ad0a5cda13f8a37a2145b0569eb1a3832a1b57
Randel M <Randel.C.Moore@gmail.com>
2022-04-27 16:28:08 -0700

    Cleans up notes at the bottom of the ApiCallObjects.cs file

--- PlateMaker/ApiCallObject.cs
+++ PlateMaker/ApiCallObject.cs
@@ -103,13 +103,16 @@ namespace PlateMaker
     }
 }
 
-// Example Search - https://skyserver.sdss.org/dr16/SkyServerWS/SearchTools/SqlSearch?cmd=select%20top%2010%20ra,dec%20from%20Frame&format=json
+///////////////
+/////Notes/////
+///////////////
 
-// URL encoding
+// Example of a general SkyServer database Search via URL - https://skyserver.sdss.org/dr17/SkyServerWS/SearchTools/SqlSearch?cmd=select%20top%2010%20ra,dec%20from%20Frame&format=json
 
+// URL Encoding
 // '+' or '%02' is ' ' (space)
 // '%3D' is '=' (equals)
-// '%2C' is ',' (equals)
+// '%2C' is ',' (comma)
 // '<' or '%3c' is '<' (less than)
 // '>' or '%3e' is '>' (greater than)
 // '%0D%0A' is (enter/return/newline)
@@ -120,75 +123,7 @@ namespace PlateMaker
 // return data format = &format=html OR &format=jsonx
 // full URL = http://skyserver.sdss.org/dr17/en/tools/search/x_results.aspx?searchtool=SQL&TaskName=Skyserver.Search.SQL&syntax=NoSyntax&ReturnHtml=true&cmd=SELECT+plate%2C+ObjId%2C+s.cx%2C+s.cy+FROM+PhotoObj+AS+p+JOIN+SpecObj+AS+s+ON+s.bestobjid+%3D+p.objid+WHERE+s.plate+<%3D+2534+AND+s.plate+>%3D+2533&format=html
 
-// Example API Calls
-
-
-//Finds all objects on a plate?!?!
-//SELECT
-//plate, specObjID
-//FROM 
-//SpecObjAll
-//WHERE
-//plate = 2534
-
-//SELECT
-//*
-//FROM 
-//SpecObjAll
-//WHERE
-//specObjID = 9262920495199967232
-//Order by xFocal asc
-
-
-
-//SELECT
-//plate, ObjId, s.cx, s.cy
-//FROM PhotoObj AS p
-//JOIN SpecObj AS s ON s.bestobjid = p.objid
-//WHERE 
-//s.plate = 2534
-
-
-//SELECT
-//plate, p.ObjId, s.cx, s.cy
-//FROM 
-//PhotoObjAll AS p
-//JOIN 
-//SpecObjAll AS s ON s.specObjID = p.specObjID
-//WHERE 
-//s.plate = 2534
-
-//"SELECT" + " " +
-//                     "plate, ObjId, xFocal, yFocal" + " " +
-//                     "FROM" + " " +
-//                     "PhotoObj AS p" + " " +
-//                     "JOIN" + " " +
-//                     "SpecObj AS s ON s.bestobjid = p.objid" + " " +
-//                     "WHERE" + " " +
-//                    $"s.plate = {plateNumber}" + " " +
-//                     "Order by xFocal asc";
-
-
-// Working option
-
-//BaseUrl = "http://skyserver.sdss.org/dr17/en/tools/search/x_results.aspx?searchtool=SQL&TaskName=Skyserver.Search.SQL&syntax=NoSyntax&ReturnHtml=true&cmd=";
-//QueryString = "" +
-//    "SELECT" + " " +
-//    "plate, ObjId, xFocal, yFocal, s.ra, s.dec" + " " +
-//    "FROM" + " " +
-//    "PhotoObj AS p" + " " +
-//    "JOIN" + " " +
-//    "SpecObj AS s ON s.bestobjid = p.objid" + " " +
-//    "WHERE" + " " +
-//   $"s.plate = {plateNumber}" + " " +
-//    "Order by xFocal asc";
-//EncodedQueryString = HttpUtility.UrlEncode(this.QueryString);
-//ContentFormat = "&format=jsonx";
-//ApiResponse = null;
-
-
-
-//Primary Keys
+//Primary Database Keys
 //SpecObjAll - primary   specObjID
 //PhotoObjAll - primary - objID
 //Plate2Target - Primary - Plate3TargetID
